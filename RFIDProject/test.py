feb13dba71c25bbe03c4359fce09baae745b68e8
ChrisZhaooo <zhaoyuhao003@gmail.com>
2023-10-11 10:19:49 +0800

    test pyqt6

--- RFIDProject/test.py
+++ RFIDProject/test.py
@@ -1,32 +1,32 @@
-# file: simple.py
-#!/usr/bin/python
-
-"""
-ZetCode PyQt6 tutorial
-
-In this example, we create a simple
-window in PyQt6.
-
-Author: Jan Bodnar
-Website: zetcode.com
-"""
-
-
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget
 
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.statusBar().showMessage('Ready')
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.clicked.connect(self.button_clicked)
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Statusbar')
+
+        self.show()
+
+    def button_clicked(self):
+        self.statusBar().showMessage('Really')
 
 def main():
-
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 200)
-    w.move(300, 300)
-
-    w.setWindowTitle('Simple')
-    w.show()
-
+    ex = Example()
     sys.exit(app.exec())
 
 
