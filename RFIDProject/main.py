ecba674a4ff94e5649bff26a2335fcbd6d214d5c
ChrisZhaooo <zhaoyuhao003@gmail.com>
2023-10-12 17:38:59 +0800

    edit main

--- RFIDProject/main.py
+++ RFIDProject/main.py
@@ -1,25 +1,5 @@
-import serial
-from time import sleep
-
-
-def recv(serial):
-    while True:
-        data = serial.read_all()
-        if data == '':
-            continue
-        else:
-            break
-        sleep(0.02)
-    return data
-
-
-def send(send_data):
-    if (serial.isOpen()):
-        serial.write(send_data.encode('utf-8'))  # 编码
-        print("发送成功", send_data)
-    else:
-        print("发送失败！")
-
+from db_connections import *
+from link_serial import *
 
 if __name__ == '__main__':
     serial = serial.Serial('COM10', 9600, timeout=0.5)
@@ -41,3 +21,15 @@ if __name__ == '__main__':
             if data.decode('ascii') != '':
                 print("receive: ", data.decode('ascii'))
                 # break
+    init_table()
+    # 调用函数来检查是否存在特定的 car 值
+    add_new_car("苏J99999", "FF1FF1")
+    car_exists = check_car_exists("苏J99999")
+
+    if car_exists:
+        print("Car exists in the database.")
+    else:
+        print("Car does not exist in the database.")
+
+    cursor.close()
+    db.close()
