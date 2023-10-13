84e3e2c013909d0b498db25415aed42c8ffa546e
ChrisZhaooo <zhaoyuhao003@gmail.com>
2023-10-13 10:00:39 +0800

    add some functions for serial operate

--- RFIDProject/link_serial.py
+++ RFIDProject/link_serial.py
@@ -3,9 +3,18 @@ from time import sleep
 import serial
 
 
-def recv(serial):
+def init_serial():
+    ser = serial.Serial('COM10', 9600, timeout=0.5)
+    if ser.isOpen():
+        print("串口打开成功")
+    else:
+        print("串口打开失败")
+    return ser
+
+
+def recv(ser):
     while True:
-        data = serial.read_all()
+        data = ser.read_all()
         if data == '':
             continue
         else:
@@ -14,9 +23,17 @@ def recv(serial):
     return data
 
 
-def send(send_data):
-    if (serial.isOpen()):
-        serial.write(send_data.encode('utf-8'))  # 编码
-        print("发送成功", send_data)
+def send(ser, send_data):
+    if ser.isOpen():
+        ser.write(send_data.encode('utf-8'))  # 编码
     else:
         print("发送失败！")
+
+
+def get_card_id(ser):
+    send(ser, "IC#")
+    while True:
+        sleep(0.5)  # 起到一个延时的效果
+        data = recv(ser)
+        if data.decode('ascii') != '':
+            return data.decode('ascii')
