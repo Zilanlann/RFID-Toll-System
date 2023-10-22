442ed53585afcadcced221cfcb7820cf2e65a720
Zilanlann <666zilan666@gmail.com>
2023-10-22 19:07:47 +0800

    rename RFID to hardware

--- RFID/UART.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UART_H__
-#define __UART_H__
-
-extern void UartInitial();
-extern void UartSend(unsigned char infor);
-extern void UartSend_String(char *Data,int len);
-
-#endif
--- RFID/delay.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DELAY_H__
-#define __DELAY_H__
-
-extern void Delay_1ms(unsigned int k);
-extern void Delay_1s(unsigned int k);
-extern void Delay_1us(unsigned int k);
-
-#endif
